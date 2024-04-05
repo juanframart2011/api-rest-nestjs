053618947358eedf25a987082a901e0dc319e4cc
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2024-04-05 00:04:49 -0600

    integrando typeORM

--- README.md
+++ README.md
@@ -71,3 +71,6 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+Documentacion de curso
+https://bluuweb.dev/nestjs/crud-mysql.html
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "api-rest-nestjs-bluuweb",
+      autoLoadEntities: true,
+      synchronize: true,
+    }),],
   controllers: [],
   providers: [],
 })
