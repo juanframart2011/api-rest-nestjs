2f31cddb7a4e5dc4c34e92a71c374fd8553cf131
juanframart2011 <juanframart2011@gmail.com>
2024-04-07 01:01:41 -0600

    auth validate with guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './guard/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -15,6 +16,12 @@ export class AuthController {
         @Body() loginDto:LoginDto
     ){
         return this.authService.login(loginDto);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get()
+    profile(){
+        return 'profile';
     }
 
     @Post('register')
