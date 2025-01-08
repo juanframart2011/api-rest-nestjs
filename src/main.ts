b25e54aee717d26964d516d910373d54de1ecc35
juanframart2011 <juanframart2011@gmail.com>
2025-01-07 22:13:48 -0600

    agregando paquete de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,17 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  //configuración de swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Blue Web')
+    .setDescription('API de ejemplo')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(parseInt(process.env.PORT) || 3000);
 }
